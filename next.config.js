8d12ed540bd8cdf1b29c67f15fa71db40346980f
sunardi1511 <sunardi1511@gmail.com>
2022-09-06 15:53:58 +0800

    first commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+const withPlugins = require("next-compose-plugins");
+const withCss = require("@zeit/next-css");
+const withReactSvg = require("next-react-svg");
+const withImages = require("next-images");
 
-module.exports = nextConfig
+const path = require("path");
+
+module.exports = withPlugins([
+  withCss({}),
+  withImages({}),
+  withReactSvg({
+    include: path.resolve(__dirname, "./public/images"),
+    webpack(config, options) {
+      return config;
+    },
+  }),
+]);
